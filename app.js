a0a4203e73eccd49bae1b7259b00d61fcb6f0a3a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-03-19 05:12:37 +0530

    page Transition method

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const sections = document.querySelectorAll(".section");
 const sectionBtns = document.querySelectorAll(".controls");
 const sectionBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function pageTransitions() {
+    for (let i = 0; i < sectionBtn.length; i++) {
+        sectionBtn[i].addEventListener("click", function(){
+            let currentBtn = document.querySelectorAll(".active-btn");
+            currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
+            this.className += " active-btn";
+        });
+    }
+}
+
+pageTransitions();
