7a8f9cd5c474da32055c2f6a88a5adad3827cbf9
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-03-20 05:27:36 +0530

    theme-toggler

--- app.js
+++ app.js
@@ -18,19 +18,25 @@ function pageTransitions() {
         const id = e.target.dataset.id;
         if(id) {
             //remove active from other btns
-            sectionBtn.forEach((btn) => {
+            sectionBtns.forEach((btn) => {
                 btn.classList.remove("active");
             });
             e.target.classList.add("active");
+            //hide other sections
+            sections.forEach((section) => {
+                section.classList.remove("active");
+            });
+    
+            const element = document.getElementById(id);
+            element.classList.add("active");
         }
+    });
 
-        //hide other sections
-        sections.forEach((section) => {
-            section.classList.remove("active");
-        });
-
-        const element = document.getElementById(id);
-        element.classList.add("active");
+    //Toggle Theme
+    const themeBtn = document.querySelector(".theme-btn");
+    themeBtn.addEventListener("click", () => {
+        let element = document.body;
+        element.classList.toggle("light-mode");
     });
 }
 
