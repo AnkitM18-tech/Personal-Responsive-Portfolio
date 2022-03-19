fb7d1753e12419297020c8861c9fc6805ce00373
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-03-19 15:34:46 +0530

    added section handling event listener

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const sectionBtn = document.querySelectorAll(".control");
 const allSections = document.querySelector(".main-content");
 
 function pageTransitions() {
+    // Click active class
     for (let i = 0; i < sectionBtn.length; i++) {
         sectionBtn[i].addEventListener("click", function(){
             let currentBtn = document.querySelectorAll(".active-btn");
@@ -11,6 +12,26 @@ function pageTransitions() {
             this.className += " active-btn";
         });
     }
+
+    // Section active class
+    allSections.addEventListener("click", (e) => {
+        const id = e.target.dataset.id;
+        if(id) {
+            //remove active from other btns
+            sectionBtn.forEach((btn) => {
+                btn.classList.remove("active");
+            });
+            e.target.classList.add("active");
+        }
+
+        //hide other sections
+        sections.forEach((section) => {
+            section.classList.remove("active");
+        });
+
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    });
 }
 
 pageTransitions();
